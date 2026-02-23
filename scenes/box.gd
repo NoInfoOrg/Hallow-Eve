9f14862a646b58b8499d9fa6d5242908f02e6003
Nicholas Lucky <nicholaslucky889@gmail.com>
2026-02-22 18:29:57 -0600

    Fix box collision and tidy up the box puzzle

--- scenes/box.gd
+++ scenes/box.gd
@@ -2,8 +2,20 @@ extends CharacterBody2D
 
 var can_move = true
 
+const MAX_PUSH_SPEED = 150.0
+
 func _physics_process(delta: float) -> void:
-	move_and_slide()
+	if (velocity[0] > MAX_PUSH_SPEED):
+		velocity = Vector2(MAX_PUSH_SPEED, velocity[1])
+	elif (velocity[0] < -MAX_PUSH_SPEED):
+		velocity = Vector2(-MAX_PUSH_SPEED, velocity[1])
+	elif (velocity[1] > MAX_PUSH_SPEED):
+		velocity = Vector2(velocity[0], MAX_PUSH_SPEED)
+	elif (velocity[1] < -MAX_PUSH_SPEED):
+		velocity = Vector2(velocity[0], -MAX_PUSH_SPEED)
+	
+	# move_and_slide() caused issues with boxes moving with Eve even when Eve is not pushing the boxes 
+	move_and_collide(velocity * delta)
 	
 	if get_slide_collision_count() == 0:
 		velocity = Vector2(0, 0)
@@ -29,3 +41,7 @@ func _physics_process(delta: float) -> void:
 
 func push_by_player(direction, push_force):
 	velocity = direction * push_force
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	print("entered")
