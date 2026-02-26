ca3e96cdbe1a7d8583bc962f389a440f8c4a3d6a
Nicholas Lucky <nicholaslucky889@gmail.com>
2026-02-26 17:35:02 -0600

    Attempt to debug the camera for level 2

--- scenes/camera_2d.gd
+++ scenes/camera_2d.gd
@@ -1,12 +1,14 @@
 extends Camera2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#enabled = true
-	#make_current()
-	pass
+	print("entered")
+	enabled = true
+	make_current()
 
+func _exit_tree() -> void:
+	print("exited")
+	enabled = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
