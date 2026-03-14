d26862ee76c1363a0a3f6e4850e129158442e283
Nicholas Lucky <nicholaslucky889@gmail.com>
2026-03-14 17:57:59 -0500

    Add functionality so the blanket ghost can have idle movements when the players are beyond a certain range

--- scenes/blanket_ghost.gd
+++ scenes/blanket_ghost.gd
@@ -13,6 +13,7 @@ const SPEED = 50.0
 
 # The distance for the blanket ghost to start being visible to the players
 const VISIBLE_THRESHOLD = 750.0
+#const VISIBLE_THRESHOLD = 200.0  # For testing
 
 # The distance for the blanket ghost to start being fully visible to the players
 const FULL_OPACITY_THRESHOLD = 150.0
@@ -23,7 +24,16 @@ const MAX_LINE_OF_SIGHT_DISTANCE = 800.0
 # Attack cooldown
 var timer = Timer.new()
 var is_in_attack_cooldown = false
+var ATTACK_COOLDOWN_TIME_SECONDS = 3.0  # 3 second cooldown?
 signal blanket_ghost_attack
+
+# Idle movement
+var idle_action_timer = Timer.new()
+var previous_idle_movement = "stop"
+var previous_velocity = Vector2(0, 0)
+const MAX_IDLE_SPEED_MULTIPLIER = 0.8
+const IDLE_STOP_TIME_SECONDS = 3.0
+const IDLE_MOVE_TIME_SECONDS = 3.0
 
 func _ready() -> void:
 	if optional_lurker:
@@ -31,7 +41,9 @@ func _ready() -> void:
 	
 	# Set up the timer for the blanket ghost's attack cooldown
 	add_child(timer)
+	add_child(idle_action_timer)
 	timer.timeout.connect(_on_timer_timeout)
+	idle_action_timer.timeout.connect(_on_idle_action_timer_timeout)
 	
 	# Turn off physics processing
 	set_physics_process(false)
@@ -43,11 +55,18 @@ func wait_for_physics():
 
 func _physics_process(delta: float) -> void:
 	var target = get_closest_target()
+	#print(get_diagonal_distance(target))
 	
-	# Only move the blanket ghost if the players are in range
+	# Only move the blanket ghost to a player if the players are in range
 	if get_diagonal_distance(target) > MAX_LINE_OF_SIGHT_DISTANCE:
 		#print("Blanket Ghost is not in range of any targets")
+		
+		# Potential idle movement
+		move_idle()
 		return
+	
+	# At this point, we assume that the blanket ghost will no longer be idle
+	reset_idle_variables()
 	
 	# Check for collision with Eve or Willow
 	for i in get_slide_collision_count():
@@ -56,12 +75,12 @@ func _physics_process(delta: float) -> void:
 		
 		if collider.get("name") in ["Eve - P1", "Willow - P2"]:
 			if not is_in_attack_cooldown:
-				print("Blanket Ghost attack on " + collider.get("name"))
+				#print("Blanket Ghost attack on " + collider.get("name"))
+				
 				blanket_ghost_attack.emit()
 				is_in_attack_cooldown = true
 				
-				# 3 second cooldown?
-				timer.wait_time = 3.0
+				timer.wait_time = ATTACK_COOLDOWN_TIME_SECONDS
 				timer.start()
 	
 	# Fix the jittering when the blanket ghost stops at the player
@@ -132,3 +151,45 @@ func set_transparency(target):
 	var offsetted_distance = diagonal_distance - FULL_OPACITY_THRESHOLD
 	var threshold_difference = VISIBLE_THRESHOLD - FULL_OPACITY_THRESHOLD
 	get_node("Sprite2D").self_modulate.a = 1 - (offsetted_distance / threshold_difference)
+
+func move_idle():
+	if idle_action_timer.get_time_left() != 0:
+		# Keep moving if the ghost is still moving in idle animation
+		if previous_idle_movement == "move":
+			#print("still moving")
+			velocity = previous_velocity
+			move_and_slide()
+			
+		return
+	
+	var rng = RandomNumberGenerator.new()
+	
+	var idle_options = ["stop", "move"]
+	previous_idle_movement = idle_options[randi_range(0, idle_options.size() - 1)]
+	#print(previous_idle_movement)
+	
+	if previous_idle_movement == "stop":
+		# Start a timer to stop for
+		idle_action_timer.wait_time = IDLE_STOP_TIME_SECONDS
+		idle_action_timer.start()
+		
+	elif previous_idle_movement == "move":
+		# Move in a random direction
+		var random_x = rng.randf_range(-MAX_IDLE_SPEED_MULTIPLIER, MAX_IDLE_SPEED_MULTIPLIER)
+		var random_y = rng.randf_range(-MAX_IDLE_SPEED_MULTIPLIER, MAX_IDLE_SPEED_MULTIPLIER)
+		
+		velocity = Vector2(random_x, random_y) * SPEED
+		previous_velocity = velocity
+		move_and_slide()
+		
+		# Start a timer to continue moving in that direction for
+		idle_action_timer.wait_time = IDLE_MOVE_TIME_SECONDS
+		idle_action_timer.start()
+
+func _on_idle_action_timer_timeout():
+	idle_action_timer.stop()
+	reset_idle_variables()
+
+func reset_idle_variables():
+	previous_idle_movement = "stop"
+	previous_velocity = Vector2(0, 0)
