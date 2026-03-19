8a7e950311e0ba5f61f2524fce002e219b3594da
Nicholas Lucky <nicholaslucky889@gmail.com>
2026-03-19 15:42:53 -0500

    Move script files in the scene folder to the script folder, and move scene files in the script folder to the scene folder

--- scripts/test_inventory.gd
+++ scripts/test_inventory.gd
@@ -1,16 +1,13 @@
-extends Node2D
+extends Resource
 
-signal updated_inv
+class_name Inv
+signal inv_updated 
 @export var items: Array[Item]
 
 func add_item(newItem: Item) -> bool:
 	items.append(newItem)
-	updated_inv.emit()
-	print("you got: ")
-	var count = 1
-	for item in items:
-		print(count, ": ", item.name)
-		count += 1
+	print("something happened btw")
+	inv_updated.emit()
 	return true
 	
 func remove_item(remItem: Item) -> bool:
@@ -18,10 +15,9 @@ func remove_item(remItem: Item) -> bool:
 	for item in items:
 		if item == remItem:
 			items.remove_at(index)
-			updated_inv.emit()
 			return true
 		index += 1
-	updated_inv.emit()
+	inv_updated.emit()
 	return false
 
 
